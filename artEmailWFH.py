59779d475b1e17f3740600ac6cc9d0171c02f2eb
Dan Whaland <46826095+danwhaland-phl@users.noreply.github.com>
2020-04-30 16:15:56 -0400

    Resolve CRSes, simplify gdfs, reformat excel doc

--- artEmailWFH.py
+++ artEmailWFH.py
@@ -4,26 +4,8 @@ from geopandas import read_file
 
 pwdParcels_gdf = read_file('https://phl.carto.com:443/api/v2/sql?q=select%20*%20from%20phl.pwd_parcels&format=GEOJSON&method=export')
 
-# see the new pwdParcels geodataframe column names. this shows that there is now a 'geometry'
-pwdParcels_gdf.head()
-
-# get the test art sites layer (which will have to be swapped out for the real art site layer) and its geometries
-import requests
-
-url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Test_Art_Sites/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=GEOJSON&token=Kd3CaU2QNHFkWVApSPuYu7TcgM5r-_0hObkgkmhlQ62is-fBfd7v8K0C20lmCY6B2rPp0buefxaJpas3ppsOIHkXLtPpCYia-FQlCI0KGtCEBdlet5zNCQqg6XB_OMEG7g8SjGcdQdwQv6coq91w-nZYSaJsRbqi-Zi4W6IK6Wf2zhSjYU11AopltMr61xwMsKI7V37Re2Onw3nJv_QDS8RNdkOwYcjui4MTvMHa8no."
-payload = {}
-headers= {}
-
-artSites = requests.request("GET", url, headers=headers, data = payload)
-
-# turn artSites to json. i think this is a necessary intermediate step, but i could be wrong. 
-artSites_json = artSites.json()
-artSites_json
-
-# this is a great step. now we actually have polygons with a recognized geometry and specifying the crs lets us project it to match the crs of pwdParcels
-import geopandas as gpd
-artSites_gdf = gpd.GeoDataFrame.from_features(artSites_json["features"], crs="EPSG:4326")
-artSites_gdf.head()
+# create an art sites geojson-based geodataframe file with geometry to compare to parcels geojson-based geodataframe file with geometry. define the crs as 4326 so it can be analyzed against the carto parcels and permits layers
+artSites_gdf = read_file('https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Test_Art_Sites/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=GEOJSON&token=Kd3CaU2QNHFkWVApSPuYu7TcgM5r-_0hObkgkmhlQ62is-fBfd7v8K0C20lmCY6B2rPp0buefxaJpas3ppsOIHkXLtPpCYia-FQlCI0KGtCEBdlet5zNCQqg6XB_OMEG7g8SjGcdQdwQv6coq91w-nZYSaJsRbqi-Zi4W6IK6Wf2zhSjYU11AopltMr61xwMsKI7V37Re2Onw3nJv_QDS8RNdkOwYcjui4MTvMHa8no.&format=GEOJSON&method=export', crs='EPSG:4326')
 
 # join intersecting pwd parcels to test art sites (needs to be swapped out for real art sites later)
 artSiteParcels = gpd.sjoin(pwdParcels_gdf, artSites_gdf, how='inner', op='intersects')
@@ -31,7 +13,6 @@ artSiteParcels
 
 # rename the index columns of the artSiteParcels so that artSiteParcels can be joined again - this time, to the permits
 artSiteParcels = artSiteParcels.rename(columns={'index_left': 'parcels_index','index_right': 'art_index'})
-artSiteParcels.head()
 
 # read the l and i permits layer to see which permits were pulled and export to geojson. make sure to change the number of days to '1 day' when the script is ready 
 liPermits_gdf = gpd.read_file("https://phl.carto.com:443/api/v2/sql?q=select%20*%20from%20phl.li_permits%20WHERE%20permitissuedate%20=%20(current_date%20-%20interval%20'82%20days')&format=GEOJSON&method=export")
@@ -41,11 +22,13 @@ liPermits_gdf
 permits_at_artSiteParcels = gpd.sjoin(artSiteParcels, liPermits_gdf, how='inner', op='intersects')
 permits_at_artSiteParcels
 
+# import pandas to create dataframes
 import pandas as pd
 
 # import time to add the date of the export to the excel document
 import time 
 
+# import os for environmnent variables
 import os
 
 # openpyxl for writing to excel
@@ -68,11 +51,11 @@ todaysDate = time.strftime('%d-%m-%Y')
 
 excelFileName = "PulledPermits_" + todaysDate + ".xlsx"
 
-# in df.to_excel part, how do i specify which columns i want to include in the excel doc and which ones i want to drop?
+# send the email
 if len(permits_at_artSiteParcels) > 0:
     print(permits_at_artSiteParcels) 
-    df = pd.DataFrame(data=permits_at_artSiteParcels)
-    df.to_excel(excelFileName, columns=['address_left','owner1','owner2','Art_Name','Address','address_right','permitdescription']) 
+    df = pd.DataFrame(data=permits_at_artSiteParcels,columns=['Address','Art_Name','address_right','permitdescription','permitnumber','address_left','owner1','owner2'])
+    df.to_excel(excelFileName, header=['Art Site Address','Art Site Name','Permit Address','Permit Description','Permit Number','Triggering Parcel Address','Triggering Parcel Owner 1','Triggering Parcel Owner 2']) 
     
     # set up email variables 
     sender = os.environ.get('DanWork_Email')
